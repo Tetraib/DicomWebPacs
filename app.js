5d88f858348b0f6f0a6607dcc0c3795a454423f0
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2015-09-22 23:28:02 +0000

    resize and compress incomming image

--- app.js
+++ app.js
@@ -1,7 +1,72 @@
-var express = require('express');
-var app = express();
-app.get('/api/', function(req, res){
-  res.send('hello world');
+var express = require('express'),
+    app = express(),
+    gcloud = require('gcloud'),
+    bodyParser = require('body-parser'),
+    mozjpeg = require('mozjpeg'),
+    sharp = require('sharp'),
+    Child_Process = require('duplex-child-process'),
+    gcs = gcloud.storage({
+        keyFilename: './CloudDicom-1f0d0f461c12.json',
+        projectId: 'axiomatic-math-616'
+    });
+app.use(bodyParser.json());
+
+// Function to make standard image from google cloud storage input image
+var makestdimage = function(fileIn, fileOut, bucket, quality, sharpen, callback) {
+// Define the bucket to use
+    var gcsbucket = gcs.bucket(bucket),
+// Resize image
+        resizeStandard = sharp()
+        .withoutEnlargement()
+        .resize(null, 1080)
+        .on('error', function(err) {
+            callback(err);
+        }),
+        cjpeg = Child_Process.spawn(mozjpeg, ['-progressive', '-optimize', '-quality', quality]),
+        // Read from gcs file
+        remoteReadStream = gcsbucket.file(fileIn).createReadStream(),
+        // Write to gcs file
+        remoteWriteStream = gcsbucket.file(fileOut).createWriteStream();
+        // Apply sharpen if asked
+    if (sharpen) {
+        resizeStandard.sharpen();
+    }
+    // Pipe image
+    remoteReadStream.pipe(resizeStandard).pipe(cjpeg).pipe(remoteWriteStream);
+    // Error management
+    remoteWriteStream.on('finish', function() {
+        callback(null);
+    });
+    cjpeg.on('error', function(err) {
+        callback(err);
+    });
+    remoteReadStream.on('error', function(err) {
+        callback(err);
+    });
+    remoteWriteStream.on('error', function(err) {
+        callback(err);
+    });
+};
+
+makestdimage('1.2.392.200036.9107.500.305.0.20150811.102846.281.100_90.jpg', 'test.jpg', 'dicom', '80', false, function(err) {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log("ok");
+    }
 });
 
-app.listen(process.env.PORT, process.env.IP);
+
+
+// 
+
+app.post('/v1/images/', function(req, res) {
+    console.log('New body : ');
+    console.log(req.body);
+    res.status('201').end();
+});
+app.put('/v1/images/:Uid/', function(req, res) {
+
+});
+// app.listen(process.env.PORT, process.env.IP);
