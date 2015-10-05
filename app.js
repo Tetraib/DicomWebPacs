ab9161247790865e86214e1a3ee9b0e47feb5a4a
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2015-10-05 21:33:56 +0000

    upload image to gcs

--- app.js
+++ app.js
@@ -11,11 +11,12 @@ var express = require('express'),
     });
 app.use(bodyParser.json());
 
-// Function to make standard image from google cloud storage input image
+
+// Function to make standard image from google cloud storage
 var makestdimage = function(fileIn, fileOut, bucket, quality, sharpen, callback) {
-// Define the bucket to use
+    // Define the bucket to use
     var gcsbucket = gcs.bucket(bucket),
-// Resize image
+        // Resize image
         resizeStandard = sharp()
         .withoutEnlargement()
         .resize(null, 1080)
@@ -27,12 +28,13 @@ var makestdimage = function(fileIn, fileOut, bucket, quality, sharpen, callback)
         remoteReadStream = gcsbucket.file(fileIn).createReadStream(),
         // Write to gcs file
         remoteWriteStream = gcsbucket.file(fileOut).createWriteStream();
-        // Apply sharpen if asked
+    // Apply sharpen if asked
     if (sharpen) {
         resizeStandard.sharpen();
     }
     // Pipe image
     remoteReadStream.pipe(resizeStandard).pipe(cjpeg).pipe(remoteWriteStream);
+
     // Error management
     remoteWriteStream.on('finish', function() {
         callback(null);
@@ -48,25 +50,33 @@ var makestdimage = function(fileIn, fileOut, bucket, quality, sharpen, callback)
     });
 };
 
-makestdimage('1.2.392.200036.9107.500.305.0.20150811.102846.281.100_90.jpg', 'test.jpg', 'dicom', '80', false, function(err) {
-    if (err) {
-        console.log(err);
-    }
-    else {
-        console.log("ok");
-    }
-});
+// makestdimage('1.2.392.200036.9107.500.305.0.20150811.102846.281.100_90.jpg', 'test.jpg', 'dicom', '80', false, function(err) {
+//     if (err) {
+//         console.log(err);
+//     }
+//     else {
+//         console.log("ok");
+//     }
+// });
 
 
 
 // 
-
 app.post('/v1/images/', function(req, res) {
-    console.log('New body : ');
-    console.log(req.body);
+    console.log('New body : ', req.body);
     res.status('201').end();
-});
-app.put('/v1/images/:Uid/', function(req, res) {
 
 });
-// app.listen(process.env.PORT, process.env.IP);
+
+app.put('/v1/images/:Uid/', function(req, res) {
+    var gcsbucket = gcs.bucket('dicom'),
+        remoteWriteStream = gcsbucket.file('test4.jpg').createWriteStream();
+
+    remoteWriteStream.on('finish', function() {
+        res.status('200').end();
+        console.log('file uploaded');
+    });
+
+    req.pipe(remoteWriteStream);
+});
+app.listen(process.env.PORT, process.env.IP);
