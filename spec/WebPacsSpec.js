2edf683a836d9c53a9dd5deacb35e1f2997225b2
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2015-10-18 01:35:42 +0000

    travis preinstall

--- spec/WebPacsSpec.js
+++ spec/WebPacsSpec.js
@@ -1,17 +1,20 @@
 var request = require("request");
 // Set Timout to 1000ms
 jasmine.getEnv().defaultTimeoutInterval = 1000;
+// var serverurl = 'https://dicomwebpacs-tetraib-1.c9.io/';
+var serverurl = 'http://localhost/';
+
 
 describe('WebPacs-API', function() {
     describe('GET /', function() {
         it('should respond with : 200', function(done) {
-            request.get('https://dicomwebpacs-tetraib-1.c9.io/', function(error, response, body) {
+            request.get(serverurl, function(error, response, body) {
                 expect(response.statusCode).toEqual(200);
                 done();
             });
         });
         it('should respond with : Running smoothly...', function(done) {
-            request.get('https://dicomwebpacs-tetraib-1.c9.io/', function(error, response, body) {
+            request.get(serverurl, function(error, response, body) {
                 expect(body).toEqual('Running smoothly...');
                 done();
             });
