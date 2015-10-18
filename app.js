2d7d3e57bdc6e7540ccd23d36199458b8739662a
Thibaut CONSTANT <thibaut.constant@isisphinx.com>
2015-10-18 17:49:43 +0000

    CI var test

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var express = require('express'),
         keyFilename: './CloudDicom-2467d1aee1c2.json',
         projectId: 'axiomatic-math-616'
     }),
-    serverAddr='https://dicomwebpacs-tetraib-1.c9.io';
+    serverAddr = 'https://dicomwebpacs-tetraib-1.c9.io';
 app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
@@ -19,25 +19,27 @@ app.get('/', function(req, res) {
 
 app.post('/v1/images/', function(req, res) {
     console.log('New body : ', req.body);
-    res.status('201').json({ 'Location': serverAddr+'/v1/images/'+req.body.SOPInstanceUID+'/'});
+    res.status('201').json({
+        'Location': serverAddr + '/v1/images/' + req.body.SOPInstanceUID + '/'
+    });
 
 });
 
 app.put('/v1/images/:Uid/', function(req, res) {
     var gcsbucket = gcs.bucket('dicom'),
-        remoteWriteStream = gcsbucket.file(req.params.Uid +'.jpg').createWriteStream(),
+        remoteWriteStream = gcsbucket.file(req.params.Uid + '.jpg').createWriteStream(),
         // Make a standard image
-        remoteWriteStream2 = gcsbucket.file(req.params.Uid +'.std.jpg').createWriteStream(),
+        remoteWriteStream2 = gcsbucket.file(req.params.Uid + '.std.jpg').createWriteStream(),
         resizeStandard = sharp()
         .sharpen()
         .withoutEnlargement()
         .resize(null, 1080)
         .on('error', function(err) {
-            console.log('sharp',err);
+            console.log('sharp', err);
         }),
         cjpeg = Child_Process.spawn(mozjpeg, ['-progressive', '-optimize', '-quality', '80']);
     cjpeg.on('error', function(err) {
-        console.log('cjpeg',err);
+        console.log('cjpeg', err);
     });
     remoteWriteStream.on('finish', function() {
         res.status('200').end();
@@ -47,10 +49,10 @@ app.put('/v1/images/:Uid/', function(req, res) {
         console.log('file low uploaded');
     });
     remoteWriteStream.on('error', function(err) {
-        console.log('remoteWriteStream',err);
+        console.log('remoteWriteStream', err);
     });
     remoteWriteStream2.on('error', function(err) {
-        console.log('remoteWriteStream2',err);
+        console.log('remoteWriteStream2', err);
     });
     req.pipe(resizeStandard).pipe(cjpeg).pipe(remoteWriteStream2);
     req.pipe(remoteWriteStream);
@@ -61,34 +63,41 @@ app.get('/v1/images/:Uid/', function(req, res) {
         remoteReadStream = gcsbucket.file(req.params.Uid + '.jpg').createReadStream();
 
     remoteReadStream.on('error', function(err) {
-        console.log('remoteReadStream',err);
+        console.log('remoteReadStream', err);
     });
     // Activate browser cache
-    res.set({'Content-Type': 'image/jpeg','Cache-Control': 'public, max-age=31557600'});
+    res.set({
+        'Content-Type': 'image/jpeg',
+        'Cache-Control': 'public, max-age=31557600'
+    });
     remoteReadStream.pipe(res);
 
 });
 
 app.get('/v1/images/:Uid/std', function(req, res) {
-       var gcsbucket = gcs.bucket('dicom'),
+    var gcsbucket = gcs.bucket('dicom'),
         remoteReadStream = gcsbucket.file(req.params.Uid + '.std.jpg').createReadStream();
 
     remoteReadStream.on('error', function(err) {
-        console.log('remoteReadStream',err);
+        console.log('remoteReadStream', err);
     });
     // Activate browser cache
-    res.set({'Content-Type': 'image/jpeg','Cache-Control': 'public, max-age=31557600'});
+    res.set({
+        'Content-Type': 'image/jpeg',
+        'Cache-Control': 'public, max-age=31557600'
+    });
     remoteReadStream.pipe(res);
 
 });
 console.log("SERVER STARTED...");
-console.log('CI',process.env.CI);
-console.log('TRAVIS',process.env.TRAVIS);
+console.log('CI', process.env.CI);
+console.log('TRAVIS', process.env.TRAVIS);
 
-// if(process.env.CI==true){
-//     console.log("IN CI...");
-  app.listen(8080);
-// }else{
-//     console.log("NOT IN CI...");
-//   app.listen(process.env.PORT, process.env.IP);
-// }
+if (process.env.CI === true) {
+    console.log("IN CI...");
+    app.listen(8080);
+}
+else {
+    console.log("NOT IN CI...");
+    app.listen(process.env.PORT, process.env.IP);
+}
